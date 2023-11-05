e373767835879100c8a00c7476a9603b0c1b1fd7
juanibencardino <jbencardinoslotowsky@frba.utn.edu.ar>
2023-11-04 22:41:54 -0300

    Test payments surgery, done and working

---
+++ domain/src/test/java/surgeries/TestPaymentsSurgery.java
@@ -0,0 +1,117 @@
+package surgeries;
+
+import domain.people.Person;
+import domain.people.SurgeryRole;
+import domain.surgery.SurgeryPaymentAsigner;
+import domain.surgery.fees.MemberBasedFee;
+import domain.surgery.practice.ParamPractice;
+import domain.surgery.ParamSurgery;
+import domain.surgery.practice.Practice;
+import domain.surgery.Surgery;
+import domain.surgery.fees.RoleBasedFee;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestPaymentsSurgery {
+    private List<Person> members;
+    private Practice practice;
+    private Surgery surgery;
+
+    @BeforeEach
+    public void init(){
+        members = this.createTestMembers();
+        practice = this.createTestPractice();
+        surgery = this.createTestSurgery(members, practice);
+    }
+
+    public List<Person> createTestMembers(){
+        Person chiefSurgeon = new Person(null, "Lucho", SurgeryRole.CHIEF_SURGERY);
+        Person anesthesist = new Person(null, "Lucho", SurgeryRole.ANESTHESIST);
+        Person auxiliarySurgeon = new Person(null, "Juani", SurgeryRole.AUXILIARY_SURGEON);
+
+        List<Person> members = new ArrayList<>();
+        members.add(anesthesist);
+        members.add(auxiliarySurgeon);
+
+        return members;
+    }
+
+    public Practice createTestPractice(){
+        RoleBasedFee auxiliarySurgeonFee = new RoleBasedFee(SurgeryRole.AUXILIARY_SURGEON, 0.2);
+        RoleBasedFee anesthesisFee = new RoleBasedFee(SurgeryRole.ANESTHESIST, 0.3);
+
+        List<RoleBasedFee> roleBasedFees = new ArrayList<>();
+
+        roleBasedFees.add(auxiliarySurgeonFee);
+        roleBasedFees.add(anesthesisFee);
+
+        ParamPractice paramPractice = new ParamPractice(); // missing list of roleBasedFees
+
+        paramPractice.setDetail("Heart transplant");
+        paramPractice.setCentre(null);
+        paramPractice.setSpecialty(null);
+        paramPractice.setPrice(100.0);
+        paramPractice.setRoleBasedFees(roleBasedFees);
+
+        Practice practice = new Practice(paramPractice);
+
+
+        return practice;
+    }
+
+    public Surgery createTestSurgery(List<Person> members, Practice practice){
+        ParamSurgery paramSurgery = new ParamSurgery();
+
+        LocalDateTime completionDate = LocalDateTime.now();
+
+        paramSurgery.setCompletionDate(completionDate);
+        paramSurgery.setMembers(members);
+        paramSurgery.setPractice(practice);
+
+        Surgery surgeryTest = new Surgery(paramSurgery);
+
+        return surgeryTest;
+    }
+
+    @Test
+    @DisplayName("The chief of surgery gets asigned the remaining amount")
+    public void chiefSurgeryPaymentAsignment(){
+
+        SurgeryPaymentAsigner.asignPayments(surgery);
+
+        Double chiefSurgeryAsigned = surgery.getChiefSurgeryFee().getAssignedAmount();
+
+        assertEquals(50.0, chiefSurgeryAsigned);
+    }
+
+    @Test
+    @DisplayName("Auxiliary surgeon gets asigned the correct amount")
+    public void asignAuxiliarySurgeonPayment(){
+        Person auxiliarySurgeon = new Person(null, "Lucho", SurgeryRole.AUXILIARY_SURGEON);
+
+        MemberBasedFee auxiliarySurgeonFee = SurgeryPaymentAsigner.asignMemberPayment(practice, auxiliarySurgeon);
+
+        Double auxiliarySurgeonAsigned = auxiliarySurgeonFee.getAssignedAmount();
+
+        assertEquals(20.0, auxiliarySurgeonAsigned);
+    }
+
+    @Test
+    @DisplayName("All members get asigned the correct amount")
+    public void asignAllMemberPayments(){
+        List<MemberBasedFee> memberBasedFees = SurgeryPaymentAsigner.asignAllMemberPayments(practice, members);
+
+        Double anesthesistAsigned = memberBasedFees.get(0).getAssignedAmount();
+        Double auxiliarySurgeonAsigned = memberBasedFees.get(1).getAssignedAmount();
+
+        assertEquals(30.0, anesthesistAsigned);
+        assertEquals(20.0, auxiliarySurgeonAsigned);
+    }
+}
